278472c05dc09f33229ae5f48e73459f98530456
Attila Csanyi <cs_ati@yahoo.com>
2014-02-06 00:08:13 +0100

    Add initial app skeleton
    Use the scaffolding feature

---
+++ README.md
@@ -0,0 +1,10 @@
+grails-payment
+======================
+
+My first simple Grails 1.3.7 webapplication
+
+Plugins
+
+* mail 1.0
+* hibernate 1.3.7
+* tomcat 1.3.7
---
+++ test/unit/paymenttracking/TransactionControllerTests.groovy
@@ -0,0 +1,17 @@
+package paymenttracking
+
+import grails.test.*
+
+class TransactionControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
